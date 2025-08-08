779749b72f084aa242dbc523aee2b142f9d01189
Sachin Nimshan <sachinnimshan@gmail.com>
2025-08-08 18:30:51 +0530

    change route names

--- src/configs/navLinks.tsx
+++ src/configs/navLinks.tsx
@@ -1,10 +1,10 @@
 export const navLinks = [
   { name: "Home", path: "/" },
-  { name: "About", path: "/about" },
-  { name: "Projects", path: "/projects" },
-  { name: "Experience", path: "/experience" },
-  { name: "Skills", path: "/skills" },
-  { name: "Academics", path: "/academics" },
-  { name: "Certifications", path: "/certifications" },
-  { name: "Contact", path: "/contact" },
+  { name: "About", path: "/about/" },
+  { name: "Projects", path: "/projects/" },
+  { name: "Experience", path: "/experience/" },
+  { name: "Skills", path: "/skills/" },
+  { name: "Academics", path: "/academics/" },
+  { name: "Certifications", path: "/certifications/" },
+  { name: "Contact", path: "/contact/" },
 ];
